b9ed18460554c29f2130a708592f02afb2743baf
Borja Rondan <44036474+borjar20@users.noreply.github.com>
2022-03-06 19:44:29 +0100

    Revert "Feature: Top nav"

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
 import '../styles/globals.css'
-import TopNav from '../components/TopNav'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <TopNav />
-      <div className='my-16'>
-        <Component {...pageProps} />
-      </div>
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
