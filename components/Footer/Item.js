9546ea8f33f3f7c64e18ad75aeb229ae27de7749
Albert0led0 <a.toledo96@gmail.com>
2022-03-07 17:23:35 +0100

    feat: Footer

--- components/Footer/Item.js
+++ components/Footer/Item.js
@@ -4,11 +4,8 @@ const Item = ({ Links, title }) => {
         <h1 className="mb-1 font-semibold">{title}</h1>
         {Links.map((link) => (
           <li key={link.name}>
-            <a
-              className="text-gray-600 hover:text-[#37798F] hover:font-bold duration-300
-            text-lg cursor-pointer leading-6"
-              href={link.link}
-            >
+            <a className="text-gray-600 hover:text-[#37798F] hover:font-bold duration-100
+            text-lg cursor-pointer leading-6" href={link.link}>
               {link.name}
             </a>
           </li>
--- components/Footer/ItemsContainer.js
+++ components/Footer/ItemsContainer.js
@@ -1,11 +1,20 @@
 import Item from "./Item";
-import { ABOUTUS, RESOURCES, RENTS} from "./Menus";
+import {ABOUTUS, RESOURCES, RENTS} from "./Menus";
 const ItemsContainer = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:px-8 px-5 py-16 border-x-4 border-[#4CA6C4] text-lg">
+    <div className="justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 border-x-4 border-t-4 border-[#4CA6C4] text-lg">
       <Item Links={ABOUTUS} title="SOBRE NOSOTROS" />
       <Item Links={RESOURCES} title="ESPACIOS" />
       <Item Links={RENTS} title="ALQUILERES" />
+      <div>
+        <span>¿Tienes un problema?</span>
+        <br/>
+        <a type="button" href="mailto:stackingup13@gmail.com" className="bg-[#4CA6C4] hover:bg-sky-400 duration-300 px-10 py-4
+        rounded-md text-white md:w-auto w-full"
+        >
+            Contáctanos
+        </a>
+      </div>
     </div>
   );
 };
