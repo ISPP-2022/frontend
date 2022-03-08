294a16e835b847d1d648ed6133f50af0d12196fe
Francisco-Borrego <fraborcar@alum.us.es>
2022-03-08 12:21:11 +0100

    feat: Add advertisement form improvement

--- components/forms/AdvertisementForm.js
+++ components/forms/AdvertisementForm.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-import house_image from '../../public/images/house.png'
-import garage_image from '../../public/images/garage.png'
-import basement_image from '../../public/images/basement.png'
-import storage_room_image from '../../public/images/storage-room.png'
-import warehouse_image from '../../public/images/warehouse.png'
-import other_image from '../../public/images/other.png'
-import paperplane_image from '../../public/images/paperplane.png'
-
-
+import room_svg from '../../public/images/room.svg'
+import garage_svg from '../../public/images/garage.svg'
+import basement_svg from '../../public/images/basement.svg'
+import storage_room_svg from '../../public/images/storage-room.svg'
+import warehouse_svg from '../../public/images/warehouse.svg'
+import other_svg from '../../public/images/other.svg'
+import paperplane_svg from '../../public/images/paperplane.svg'
+import tag_svg from '../../public/images/tag.svg'
+import image_svg from '../../public/images/image.svg'
+import { useState } from 'react'
 
 export default function AdvertisementForm() {
+
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div>
-        <main className='grid bg-blue-200 place-items-center py-4 md:w-screen'>
-            <form className='bg-white text-blue-400 p-6 rounded-xl space-y-4 divide-y-2'>
+        <main className='grid bg-webcolor-50 place-items-center md:py-4 md:w-screen'>
+            <form className='bg-white text-webcolor-50 p-6 md:rounded-xl space-y-4 divide-y-2'>
                 <p className='text-center'>INFORMACIÓN DE TU ESPACIO</p>
                 <fieldset className='space-y-4'>
                     <p className='py-4'>Tipo de espacio</p>
@@ -22,7 +26,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="room" name="space" value="room" />
                             <label htmlFor="room" className='flex justify-center rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={house_image} width="100" height="100" />
+                                <Image src={room_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Habitación</p>
                         </li>
@@ -30,7 +34,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="garage" name="space" value="garage" />
                             <label htmlFor="garage" className='flex justify-center  rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={garage_image} width="100" height="100" />
+                                <Image src={garage_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Garaje</p>
                         </li>
@@ -38,7 +42,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="basement" name="space" value="basement" />
                             <label htmlFor="basement" className='flex justify-center rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={basement_image} width="100" height="100" />
+                                <Image src={basement_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Sótano</p>
                         </li>
@@ -46,7 +50,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="storage-room" name="space" value="storage-room" />
                             <label htmlFor="storage-room" className='flex justify-center rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={storage_room_image} width="100" height="100" />
+                                <Image src={storage_room_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Trastero</p>
                         </li>
@@ -54,7 +58,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="warehouse" name="space" value="warehouse" />
                             <label htmlFor="warehouse" className='flex justify-center rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={warehouse_image} width="100" height="100" />
+                                <Image src={warehouse_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Almacén</p>
                         </li>
@@ -62,7 +66,7 @@ export default function AdvertisementForm() {
                         <li>
                             <input className='hidden peer' type="radio" id="other" name="space" value="other" />
                             <label htmlFor="other" className='flex justify-center rounded-xl hover:bg-slate-100 peer-checked:bg-slate-200'>
-                                <Image src={other_image} width="100" height="100" />
+                                <Image src={other_svg} width="100" height="100" />
                             </label>
                             <p className='flex justify-center'>Otro</p>
                         </li>
@@ -73,18 +77,30 @@ export default function AdvertisementForm() {
                     <p className='py-4'>Tipo de alquiler</p>
                     <ul className='grid grid-cols-3'>
                         <li className='border-2'>
-                            <input className='hidden peer' type="radio" id="hours" name="type" value="hours" />
+                            <input className='hidden peer' type="radio" id="hours" name="type" value="hours" onChange={(e) => setIsChecked(true)} />
                             <label htmlFor="hours" className='flex justify-center hover:bg-slate-100 peer-checked:bg-slate-200'>Horas</label>
                         </li>
                         <li className='border-2 text-center'>
-                            <input className='hidden peer' type="radio" id="days" name="type" value="days" />
+                            <input className='hidden peer' type="radio" id="days" name="type" value="days" onChange={(e) => setIsChecked(false)} />
                             <label htmlFor="days" className='flex justify-center hover:bg-slate-100 peer-checked:bg-slate-200'>Días</label>
                         </li>
                         <li className='border-2 text-center'>
-                            <input className='hidden peer' type="radio" id="months" name="type" value="months" />
+                            <input className='hidden peer' type="radio" id="months" name="type" value="months" onChange={(e) => setIsChecked(false)} />
                             <label htmlFor="months" className='flex justify-center hover:bg-slate-100 peer-checked:bg-slate-200'>Meses</label>
                         </li>
                     </ul>
+
+                    <div className='md:grid md:grid-cols-2'>
+                        <div className='py-4'>
+                            <label htmlFor="start_hour" className='pr-4'>Hora de inicio</label>
+                            <input className='border' disabled={!isChecked} type="time" id="start_hour"/>
+                        </div>
+                        <div className='py-4'>
+                            <label htmlFor='end_hour' className='pr-9 md:pr-4'>Hora de fin</label>
+                            <input className='border' disabled={!isChecked} type="time" id="end_hour"/>
+                        </div>
+                    </div>
+
                 </fieldset>
 
                 <fieldset className='grid grid-cols-3 gap-3 pt-4 '>
@@ -99,7 +115,7 @@ export default function AdvertisementForm() {
 
                 <fieldset className='space-y-4 space-x-4'>
                     <label htmlFor="location">Ubicación</label>
-                    <input className="border" type="text" id="location" size="30" />
+                    <input className="border" type="text" id="location" />
                 </fieldset>
 
                 <fieldset className='flex flex-row'>
@@ -126,22 +142,26 @@ export default function AdvertisementForm() {
                 </fieldset>
 
                 <fieldset className='space-y-4 space-x-4'>
-                    <label htmlFor="tags">Etiquetas</label>
+                    <label htmlFor="tags">
+                        <Image src={tag_svg} width="35"  height="35" />    
+                    </label>
                     <select id="tags" multiple>
                         <option value="1">Etiqueta 1</option>    
                         <option value="2">Etiqueta 2</option>
                     </select>
                 </fieldset>
 
-                <fieldset className='space-y-4 space-x-4'>
-                    <label>Imágenes</label>
-                    <input type="file" multiple id="img" name="img" accept="image/*" />
+                <fieldset className='space-x-4 pt-4 flex flex-row'>
+                    <label className='inline-block' htmlFor='img'>
+                        <Image src={image_svg} width='35' height='35' />    
+                    </label>
+                    <input className='pt-0' type="file" multiple id="img" name="img" accept="image/*" />
                 </fieldset>
 
                 <div className='pt-10 flex items-center justify-center'>
-                    <button className=' border-2 p-2 border-indigo-700' type="submit">
+                    <button className='border-2 p-2 border-webcolor-50 hover:bg-slate-100' type="submit">
                         <span className='pr-2'>Publicar</span>
-                        <Image src={paperplane_image} width="15" height="15"/>
+                        <Image src={paperplane_svg} width="15" height="15"/>
                     </button>
                 </div>
             </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        webcolor: {
+          50: '#4aa7c0'
+        }
+      }
+    }
   },
   plugins: [],
 }
