30678c26dbeab8c64bac0f4b04d03f9d0fd05f3d
borjar20 <borjar20@gmail.com>
2022-03-15 18:17:56 +0100

    feat: Logica modal auth

--- components/TopNav/index.js
+++ components/TopNav/index.js
@@ -1,11 +1,24 @@
 import Image from "next/image"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link"
 import AuthModal from "../AuthModal";
+import axios from "axios";
 
-function Navbar() {
+function Navbar({ user }) {
   const [isOpen, setIsOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    setIsLogged(user);
+  }, [user]);
+
+  const handleLogout = () => {
+    axios.post(`${process.env.AUTH_API_URL || 'http://localhost:4000'}/api/v1/logout`, {}, { withCredentials: true })
+      .then(res => {
+        setIsLogged(false);
+      })
+  }
+
   return (
     <nav className="bg-gray-100 fixed top-0 inset-x-0 h-16 z-50">
       <section className="shadow-md mx-auto px-4">
@@ -13,14 +26,14 @@ function Navbar() {
           {/* Logos */}
           <Link href="/" passHref>
             <a className="h-16 w-16 cursor-pointer mr-4 shrink-0 relative md:hidden">
-              <div>
+              <div className="relative h-full w-full">
                 <Image priority src="/logo.png" alt="StackingUp Logo" layout="fill" />
               </div>
             </a>
           </Link>
           <Link href="/" passHref>
             <a className="h-16 w-[181px] cursor-pointer relative md:block hidden">
-              <div className="">
+              <div className="relative h-full w-full">
                 <Image priority src="/logolargo.png" alt="StackingUp Logo" layout="fill" />
               </div>
             </a>
@@ -31,16 +44,29 @@ function Navbar() {
             py-2 px-4 block appearance-none leading-normal my-2 w-full max-w-md
             transition duration-200 ease-in-out md:hidden"
             type="text" placeholder="Search" />
-
           {/* Botones vista navegador */}
-          <div className="mr-5 align-middle md:flex hidden">
-            <button onClick={() => setShowModal(true)} className="text-white bg-[#4aa7c0] px-5 py-1 text-xl my-auto rounded hover:bg-[#34778a] transition-colors duration-100 font-semibold flex items-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-              </svg>
-              <p>Conectar</p>
-            </button>
-          </div>
+
+          {
+            isLogged ? (
+              <div className="mr-5 align-middle md:flex hidden">
+                <button onClick={handleLogout} className="text-white bg-[#4aa7c0] px-5 py-1 text-xl my-auto rounded hover:bg-[#34778a] transition-colors duration-100 font-semibold flex items-center space-x-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                  </svg>
+                  <p>Desconectar</p>
+                </button>
+              </div>) : (
+              <div className="mr-5 align-middle md:flex hidden">
+                <button onClick={() => setShowModal(true)} className="text-white bg-[#4aa7c0] px-5 py-1 text-xl my-auto rounded hover:bg-[#34778a] transition-colors duration-100 font-semibold flex items-center space-x-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                  </svg>
+                  <p>Conectar</p>
+                </button>
+              </div>
+            )
+          }
+
           {/* Menu movil */}
           <div className="md:hidden flex items-center ml-4">
             <button onClick={() => setIsOpen(!isOpen)} className="mobile-menu-button">
@@ -68,10 +94,17 @@ function Navbar() {
           <section className="row-span-4 text-center space-y-5">
             <div className="mx-6 border-t-2" />
             {/* Botones movil */}
+            {
+              isLogged ? (
+                <button onClick={handleLogout} className="text-white bg-[#4aa7c0] w-4/5 px-5 py-2 text-2xl align-middle space-x-4 my-2 rounded hover:bg-[#34778a] font-semibold transition-colors duration-100">
+                  <p>Desconectar</p>
+                </button>) : (
+                <button onClick={() => setShowModal(true)} className="text-white bg-[#4aa7c0] w-4/5 px-5 py-2 text-2xl align-middle space-x-4 my-2 rounded hover:bg-[#34778a] font-semibold transition-colors duration-100">
+                  <p>Conectar</p>
+                </button>
+              )
+            }
 
-            <button onClick={() => setShowModal(true)} className="text-white bg-[#4aa7c0] w-4/5 px-5 py-2 text-2xl align-middle space-x-4 my-2 rounded hover:bg-[#34778a] font-semibold transition-colors duration-100">
-              Conectar
-            </button>
 
           </section>
           <section className="row-span-4 space-y-0.5">
@@ -101,7 +134,7 @@ function Navbar() {
         </div>
       </section>
       {showModal && (
-        <AuthModal handleClose={() => setShowModal(false)} />
+        <AuthModal setIsLogged={setIsLogged} handleClose={() => setShowModal(false)} />
       )}
     </nav >
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,33 @@
 import '../styles/globals.css'
 import '../styles/SpacesCarousel.css';
 import TopNav from '../components/TopNav'
-
+import App from 'next/app'
+import jwt from 'jsonwebtoken';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <TopNav />
+      <TopNav user={pageProps.user} />
       <div className='mt-16'>
         <Component {...pageProps} />
       </div>
     </>
   )
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  const authToken = appContext.ctx.req ? appContext.ctx.req.cookies.authToken : null;
+  if (authToken) {
+    try {
+      appProps.pageProps.user = jwt.decode(authToken);
+    } catch (error) {
+      console.log('Invalid token');
+    }
+  }
+  return { ...appProps }
 }
 
 export default MyApp
