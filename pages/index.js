011f46fe92aac5cda7af22cf613b7427f9eeeb1c
borjar20 <borjar20@gmail.com>
2022-03-19 23:41:20 +0100

    Icon change

--- pages/index.js
+++ pages/index.js
@@ -35,14 +35,14 @@ export default function Home() {
           <div className="imageBackground md:row-span-2 lg:col-span-2 h-full">
             {/* SmartSearch*/}
             <div className="flex justify-end m-3 top-1/5 left-1/5">
-                <button 
+              <button
                 onClick={handleSmart}
                 className="rounded-full bg-white transition duration-150 ease-in-out border-2 border-blue-bondi hover:border-blue-bondi-dark hover:bg-blue-bondi-dark text-blue-bondi hover:text-white font-bold py-5 px-8 focus:outline-none focus:shadow-outline">
-                  Smart Search
-                  <svg className="float-right ml-2 h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                </button> 
+                Smart Search
+                <svg className="float-right h-6 w-6 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+                </svg>
+              </button>
             </div>
             <div className="top-1/3 left-1/4 relative">
               <div className="w-3/5 opacity-85">
