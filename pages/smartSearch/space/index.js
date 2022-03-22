fd6535473d5c3006b4382c9341cefe919b5e0126
borjar20 <borjar20@gmail.com>
2022-03-22 18:21:06 +0100

    feat: Session storage for smartSearch

--- pages/smartSearch/space/index.js
+++ pages/smartSearch/space/index.js
@@ -15,13 +15,12 @@ export async function getServerSideProps(ctx) {
 
     const user = ctx.req.cookies.authToken ? jwt.decode(ctx.req.cookies.authToken) : null;
 
-    let items;
+    let items = [];
     if (user) {
         let itemsdata = await axios.get(`${process.env.DATA_API_URL || 'http://localhost:4100'}/api/v1/users/${user.userId}/items`);
-        items = itemsdata.data;
+        items = itemsdata.data || [];
     }
 
-    items = items || [];
     return {
         props: {
             datadimensions,
@@ -73,11 +72,10 @@ function Space({ datadimensions, dataTypes, user, items }) {
                 let result = await axios.post(`${process.env.NEXT_PUBLIC_DATA_API_URL || 'http://localhost:4100'}/api/v1/smartSearch/spaces?latitude=${lat}&longitude=${lon}`, data, {
                     withCredentials: true
                 })
+                sessionStorage.setItem("smartSearch", JSON.stringify(result.data));
                 router.push({
                     pathname: `/smartSearch/spaces/results`,
-                    query: {
-                        data: result.data?.map(item => item.id)
-                    },
+
                 });
             }, function (error) {
                 alert('Es necesario activar la geolocalización para poder acceder a esta funcion');
@@ -92,66 +90,97 @@ function Space({ datadimensions, dataTypes, user, items }) {
                     <h1 className=" text-2xl md:text-3xl flex justify-center items-center font-bold text-[#4aa7c0] ">¿Qué quieres guardar?</h1>
                 </div>
                 <div className="grid grid-cols-1 justify-items-center">
-                    <div className="w-[95vw] md:w-full">
-                        <table className="md:min-w-full w-[100%]">
-                            <tbody>
-                                {data.map((obj, index) => {
-                                    return (<tr key={index} id={index + '-tr'} className="border-b odd:bg-[#d8ecf4] even:bg-white  odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
-                                        <td className="py-4 px-6 text-sm text-gray-500 dark:text-gray-400 flex items-center justify-center">
-                                            <button onClick={() => {
-                                                let arr = [...data];
-                                                arr.splice(index, 1);
-                                                setData(arr);
-                                            }}>
-                                                <img src="/images/cross.svg" width="14px" ></img>
-                                            </button>
-                                        </td>
-                                        <td className="py-4 text-xl font-medium text-gray-900 whitespace-nowrap">
-                                            {enumTranslator.types[obj.type]}
-                                        </td>
-                                        <td className="py-4 text-xl font-medium text-gray-900 whitespace-nowrap">
-                                            {enumTranslator.dimensions[obj.dimensions]}
-                                        </td>
-                                    </tr>)
-                                })}
-
-                                <tr className="border-b odd:bg-[#d8ecf4] even:bg-white odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
-                                    <td className="py-4 px-6 flex items-center justify-center">
-                                        <button id="plus" onClick={a1}>
-                                            <img src={buttonPlusPressedIMG} width="25px" className=" fill-[#4aa7c0] "></img>
-                                        </button>
-                                    </td>
-                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
-
-                                    </td>
-                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap ">
-
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <br></br>
-                    {buttonPlusPressed &&
-                        <div className="grid grid-cols-1 justify-items-center">
-                            <div id="form" className="md:max-w-[95%] rounded-xl border-2 mb-8 border-[#4aa7c0] ">
-                                <form onSubmit={addObject}>
-                                    <div className="flex flex-col md:flex-row items-center justify-center">
-                                        <select id="type" className="form-select appearance-none border border-solid border-[#4aa7c0] rounded transition ease-in-out m-5 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
-                                            {dataTypes.map(obj => { return (<option key={obj} value={obj} >{enumTranslator.types[obj]}</option>) })}
-                                        </select>
-
-                                        <select id="dimensions" className="form-select appearance-none border border-solid border-[#4aa7c0] rounded transition ease-in-out m-5 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
-                                            {datadimensions.map(obj => { return (<option key={obj} value={obj} >{enumTranslator.dimensions[obj]}</option>) })}
-                                        </select>
-
-                                    </div>
-                                    <div className="flex flex-row items-center justify-center">
-                                        <button type="submit" className="text-white bg-[#4aa7c0]  focus:ring-4  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Add</button>
-                                    </div>
-                                </form>
+                    <div className="w-[95vw] md:w-full relative">
+                        <div className="has-tooltip absolute -top-4 -left-4 ">
+                            <div class="tooltip rounded shadow-lg p-4 border-[#4aa7c0] border bg-white w-[300px] top-5 left-5">
+                                <table className="w-full">
+                                    <thead>
+                                        <tr>
+                                            <th className="text-center">Dimension</th>
+                                            <th className="text-center">Tamaño aprox.</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="border-t border-blue-bondi">
+                                        <tr>
+                                            <th className="text-base font-normal">Muy grande</th>
+                                            <th className="text-base font-normal">&gt;2 &#13217;</th>
+                                        </tr>
+                                        <tr>
+                                            <th className="text-base font-normal">Grande</th>
+                                            <th className="text-base font-normal">1-2 &#13217;</th>
+                                        </tr>
+                                        <tr>
+                                            <th className="text-base font-normal">Mediano</th>
+                                            <th className="text-base font-normal">0,5-1 &#13217;</th>
+                                        </tr>
+                                        <tr>
+                                            <th className="text-base font-normal">Pequeño</th>
+                                            <th className="text-base font-normal">&lt;0,5 &#13217;</th>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                        </div>}
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-bondi" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                        <div className="md:min-w-full w-[100%]">
+                            <div className="items-center justify-between overflow-y-auto w-full" style={{ maxHeight: '30vh' }}>
+                                {data.map((obj, index) => {
+                                    return (
+                                        <div key={index} id={index + '-tr'} className="h-16 flex border-b odd:bg-[#d8ecf4] even:bg-white  odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
+                                            <div className="py-4 basis-1/5 px-6 text-sm text-gray-500 dark:text-gray-400 flex items-center justify-center">
+                                                <button onClick={() => {
+                                                    let arr = [...data];
+                                                    arr.splice(index, 1);
+                                                    setData(arr);
+                                                }}>
+                                                    <img src="/images/cross.svg" width="14px" ></img>
+                                                </button>
+                                            </div>
+                                            <div className="py-4 flex basis-2/5 text-xl font-medium text-gray-900 whitespace-nowrap justify-center">
+                                                {enumTranslator.types[obj.type]}
+                                            </div>
+                                            <div className="py-4 flex basis-2/5 text-xl font-medium text-gray-900 whitespace-nowrap justify-center">
+                                                {enumTranslator.dimensions[obj.dimensions]}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                            <div className="border-b odd:bg-[#d8ecf4] even:bg-white odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
+                                <td className="py-4 px-6 flex items-center justify-center">
+                                    <button id="plus" onClick={a1}>
+                                        <img src={buttonPlusPressedIMG} width="25px" className=" fill-[#4aa7c0] "></img>
+                                    </button>
+                                </td>
+                                {buttonPlusPressed &&
+                                    <div className="grid grid-cols-1 justify-items-center">
+                                        <div id="form" className="w-2/3 md:w-4/5 rounded-xl border mb-8 border-[#4aa7c0] ">
+                                            <form onSubmit={addObject}>
+                                                <div className="flex flex-col sm:flex-row items-center justify-center p-3 space-y-2">
+                                                    <select id="type" className="form-select appearance-none border border-solid border-[#4aa7c0] mx-2 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                                                        {dataTypes.map(obj => { return (<option key={obj} value={obj} >{enumTranslator.types[obj]}</option>) })}
+                                                    </select>
+
+                                                    <select id="dimensions" className="form-select appearance-none border border-solid border-[#4aa7c0] mx-2 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                                                        {datadimensions.map(obj => { return (<option key={obj} value={obj} >{enumTranslator.dimensions[obj]}</option>) })}
+                                                    </select>
+
+                                                </div>
+                                                <div className="flex flex-row items-center justify-center">
+                                                    <button type="submit" className="text-white bg-[#4aa7c0] focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Add</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+
+                    <br></br>
+
                     <div className="flex flex-row justify-center items-center">
                         <button onClick={() => handleStart()} disabled={data.length === 0} className={`${data.length === 0 ? 'bg-gray-300 text-gray-400' : 'bg-blue-bondi hover:bg-blue-bondi-dark'} text-white font-bold py-2 px-4 rounded`}>
                             Start
