bf604bd3b41a8748f5e085312462b11c16ff8c85
Borja Rondan <44036474+borjar20@users.noreply.github.com>
2022-03-23 18:26:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (search.trim().length > 3)
       router.push(`/search?search=${search.trim()}`);
   };
