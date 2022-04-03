a7478ca32031d4d79daa18003745c9df339a551e
serarirud <sergiariasruda@gmail.com>
2022-04-03 19:14:25 +0200

    feat: Component Comments finished.
    Now an user can post a rating comment if logged in.

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -2,49 +2,70 @@ import { Comments } from "../../components/Comments";
 import { Subtitle, Title } from "../../components/Core/Text";
 import Head from "next/head";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 
-/** A partir del context proporcionado por getServerSideProps(context), 
+/** A partir del context proporcionado por getServerSideProps(context),
  * saca el id del usuario y obtiene las reseñas que se le han hecho.
- * Devuelve las reseñas y un objeto cuyas claves son los id de las reseñas 
+ * Devuelve las reseñas y un objeto cuyas claves son los id de las reseñas
  * y los valores son el nombre completo del usuario que realizó la reseña
- * 
- * @param {*} param0 
+ *
+ * @param {*} param0
  * @returns {object} data
  */
-async function getRatingComponentData({query}){
-  const ratings = await axios.get(
-    `${process.env.DATA_API_URL}/api/v1/users/${query.id}/ratings`
-  );
+async function getRatingComponentData({ query }) {
+  const ratings = await axios
+    .get(`${process.env.DATA_API_URL}/api/v1/users/${query.id}/ratings`)
+    .catch(() => {
+      return { data: [] };
+    });
 
   const reviewers = {};
 
-  for(var id in ratings.data){
-    const rating = ratings.data[id]
-    const user = await axios.get(
-      `${process.env.DATA_API_URL}/api/v1/users/${rating.reviewerId}`
-    );
+  ratings.data = ratings.data.filter((value) => {
+    return value.reviewerId !== parseInt(query.id);
+  });
+
+  for (var id in ratings.data) {
+    const rating = ratings.data[id];
+    const user = await axios
+      .get(`${process.env.DATA_API_URL}/api/v1/users/${rating.reviewerId}`)
+      .catch(() => {
+        return { data: { id: -1, name: "Usuario", surname: "Anónimo" } };
+      });
     const ratingId = rating.rating;
-    reviewers[ratingId] = user.data.name + " " + user.data.surname;
+    reviewers[ratingId] = {};
+    reviewers[ratingId].userName = user.data.name + " " + user.data.surname;
+    reviewers[ratingId].userId = user.data.id;
   }
   return { ratings: ratings.data || [], reviewers: reviewers || [] };
 }
 
-export function getServerSideProps(context) {
+export async function getServerSideProps(context) {
+  const cookies = context.req.cookies;
+  const user = jwt.decode(cookies.authToken);
+  const isLogged = user !== null;
+  const prp = await getRatingComponentData(context);
+  prp.loggedIn = isLogged;
+  prp.loggedUserId = user === null ? -1 : user.userId;
+  prp.userId = context.query.id
   return {
-    props: getRatingComponentData(context),
+    props: prp,
   };
 }
 
 function User(props) {
   const { ratings } = props;
   const { reviewers } = props;
+  const { loggedIn } = props;
+  const { userId } = props;
+  const { loggedUserId } = props;
   return (
     <div>
       <Head>
         <title>Usuario</title>
       </Head>
       <Title> Página del usuario ª</Title>
-      <Comments ratings={ratings} reviewers={reviewers}/>
+      <Comments userId={userId} loggedUserId={loggedUserId} ratings={ratings} reviewers={reviewers} userUrl="/user/${id}" loggedIn={loggedIn} />
     </div>
   );
 }
