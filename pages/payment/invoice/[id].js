81ed704fc7ede49de5f001091afbbdf3d0b68bc3
borjar20 <borjar20@gmail.com>
2022-04-05 23:38:45 +0200

    feat: Invoice viewer

--- pages/payment/invoice/[id].js
+++ pages/payment/invoice/[id].js
@@ -1,141 +1,42 @@
 import React from 'react'
-import niceInvoice from 'nice-invoice';
 import Invoice from '../../../components/Payment/Invoice';
 import jwt from 'jsonwebtoken'
-import axios from 'axios';
+import Head from 'next/head';
 
 
 function invoice(props) {
-   
+
   return (
     <>
-    <Invoice rentId={props.rentId}/>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,500;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </Head>
+      <Invoice user={props.user} />
     </>
   )
 }
 
-export async function getServerSideProps(ctx) {  
+export async function getServerSideProps(ctx) {
 
-    const rentalId = ctx.params.id;
-    const cookies = ctx.req.cookies;
-    const user = jwt.decode(cookies.authToken);
-    
-    // Si no está logueado, redirige a la página principal
-    if (user===null) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            }
-        }
-    }
+  const cookies = ctx.req.cookies;
+  const user = jwt.decode(cookies.authToken);
 
-    let rental = null;
-    let space = null;
-    let renter = null;
-    let owner = null;
-
-    const result = await axios.get(`${process.env.DATA_API_URL || 'http://localhost:4100'}/api/v1/rentals/${rentalId}`)
-    .then(res => {
-        rental = res.data;
-    }).catch(err => {
-      // Si no existe el rental, lleva a la página principal
-      return {
-        redirect: {
-          destination: "/",
-          permanent: false,
-        }
-      }
-    })
-    
-    if (rental !== null) {
-      const result2 = await axios.get(`${process.env.DATA_API_URL || 'http://localhost:4100'}/api/v1/spaces/${rental.spaceId}`)
-      .then(res => {
-        space = res.data;
-      }).catch(err => {
-        console.log("Error retrieving the space")
-      });
-
-      const result3 = await axios.get(`${process.env.DATA_API_URL || 'http://localhost:4100'}/api/v1/users/${rental.renterId}`)
-      .then(res => {
-        renter = res.data;
-      }).catch(err => {
-        console.log("Error retrieving the renter user")
-      });
-
-      // Si el usuario que accede no es el mismo que el que alquila, redirige a la página principal.
-      if (user.userId!==rental.renterId) {
-          return {
-              redirect: {
-                  destination: "/",
-                  permanent: false,
-              }
-          }
-      } 
-
-      const result4 = await axios.get(`${process.env.DATA_API_URL || 'http://localhost:4100'}/api/v1/users/${space.ownerId}`)
-      .then(res => {
-        owner = res.data;
-      }).catch(err => {
-        console.log("Error retrieving the owner user")
-      });
-
-    } else {
-      return {
-        redirect: {
-            destination: "/",
-            permanent: false,
-        }
-      }
-    }
-    
-    let costWithoutIva = rental.cost / 1.21;
-
-    const invoiceDetail = {
-        shipping: {
-        name: renter.name + " " + renter.surname,
-        address: "Desconocido",
-        city: "Desconocido",
-        state: "Desconocido",
-        country: "España",
-        postal_code: "Desconocido"
-        },
-        items: [
-        {
-            item: "Espacio",
-            description: space.name,
-            owner: owner.name + " " + owner.surname,
-            price: costWithoutIva , 
-            quantity: 1,
-            tax: "21 % (IVA)",
-        }
-        ],
-        subtotal: rental.cost,
-        total: rental.cost,
-        order_number: rental.id,
-        header:{
-            company_name: "Reserva de espacio",
-            company_logo: "public/logo.png",
-            company_address: "Stacking Up           Avenida Reina Mercedes, Sevilla, 41012"
-        },
-        footer:{
-        text: "El coste unitario incluye las comisiones de StackingUp. Contacto: stackingup13@gmail.com"
-        },
-        currency_symbol:"€", 
-        date: {
-        billing_date: new Date().toLocaleDateString(),
-        due_date: "No procede",
-        }
-    };
-
-    niceInvoice(invoiceDetail, `public/invoices/${rentalId}.pdf`);
-
+  // Si no está logueado, redirige a la página principal
+  if (user === null) {
     return {
-        props: {
-            rentId: rentalId,
-        }
+      redirect: {
+        destination: "/",
+        permanent: false,
+      }
     }
+  }
+
+  return {
+    props: {
+      user: user,
+    }
+  }
 }
-  
+
 
 export default invoice
