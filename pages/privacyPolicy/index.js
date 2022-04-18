05acbedf78208e0d7eb6f25049fda184258d4e56
Alejandro Morales Garcia <alemorgar4@alum.us.es>
2022-04-18 20:55:45 +0200

    feat: Page for Privacy Policy done

---
+++ pages/privacyPolicy/index.js
@@ -0,0 +1,205 @@
+import Footer from "../../components/Footer";
+import Head from "next/head";
+
+export default function privacyPolicy() {
+
+    return (
+        <div>
+            <div className="md:bg-gray-100 flex justify-center items-center">
+                <Head>
+                    <title>Política de Privacidad</title>
+                </Head>
+                <main className="md:bg-white p-4 md:mb-14 md:w-2/3 md:mt-3  md:rounded-xl md:border-2 md:border-[#4aa7c0] flex flex-col">
+                    <div className="md:block p-8 mb-5">
+                        <h1 className="text-5xl flex justify-center items-center font-bold text-[#4aa7c0] ">Política de Privacidad</h1>
+                    </div>
+                   
+                    <div className="flex justify-center items-center">
+                        <div className="w-[90%] text-justify" >
+                            <h2 className="flex font-bold text-[#256172] mb-2" >Protección de datos de carácter personal según la LOPD </h2>
+                            <p> Stacking Up, en aplicación de la normativa vigente en materia de protección de datos de carácter personal, informa que los datos personales que se recogen
+                                a través de los formularios del sitio web: stackingup.es o de la aplicación móvil, se incluyen en los ficheros automatizados específicos de usuarios de 
+                                los servicios de Stacking Up.</p>
+                            <br></br>
+                            <p>La recogida y tratamiento automatizado de los datos de carácter personal tiene como finalidad el mantenimiento de la 
+                                relación comercial y el desempeño de tareas de información, formación, asesoramiento y otras actividades propias de Stacking Up.</p>
+                            <br></br>
+                            <p>Estos datos únicamente serán cedidos a aquellas entidades que sean necesarias con el único objetivo de dar cumplimiento a la finalidad anteriormente 
+                                expuesta.</p>
+                            <br></br>
+                            <p>Stacking Up adopta las medidas necesarias para garantizar la seguridad, integridad y confidencialidad de los datos conforme a lo dispuesto en el 
+                                Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que 
+                                respecta al tratamiento de datos personales y a la libre circulación de estos.</p>
+                            <br></br>
+                            <p>El usuario podrá en cualquier momento ejercitar los derechos de acceso, oposición, rectificación y cancelación reconocidos en el citado Reglamento (UE). 
+                                El ejercicio de estos derechos puede realizarlo el propio usuario a través de email a: support@stackingup.es, indicando qué desea realizar. Las 
+                                respuestas a estas solicitudes tendrán un margen de respuesta de 48 horas.</p>
+                            <br></br>
+                            <p>El usuario manifiesta que todos los datos facilitados por él son ciertos y correctos, y se compromete a mantenerlos actualizados, comunicando los cambios
+                                a support@stackingup.es. </p>
+                            <br></br>
+                            <br></br>
+                            <h2 className="flex font-bold text-[#256172] " >Finalidad del tratamiento de los datos personales: </h2>
+                            <h3 className="flex font-bold italic text-[#256172] mb-2">¿Con qué finalidad trataremos tus datos personales? </h3>
+                            <p>En Stacking Up, trataremos tus datos personales recabados a través del Sitio Web stackingup.es o la aplicación móvil, con las siguientes 
+                                finalidades: </p>
+                                <div className="flex justify-center items-center">
+                                    <div className="w-[90%] text-justify" >
+                                        <li>En caso de contratación de los bienes y servicio ofertados a través de stackingup.es o de la
+                                        aplicación móvil, para mantener la relación  contractual, así como la gestión, administración, información, prestación y mejora del servicio. </li>
+                                        <li>Envío de información solicitada a través de los formularios dispuestos en stackingup.es o de la aplicación móvil. </li>
+                                        <li>Remitir boletines (newsletters), así como comunicaciones comerciales de promociones y/o publicidad de Stacking Up y del sector. </li>
+                                     </div>
+                                </div>
+                            
+                            <br></br>
+                            <p>Te recordamos que puedes oponerte al envío de comunicaciones comerciales por cualquier vía y en cualquier momento, remitiendo un correo 
+                                electrónico a la dirección indicada anteriormente.  </p>
+                            <br></br>
+                            <p>Los campos de dichos registros son de cumplimentación obligatoria, siendo imposible realizar las finalidades expresadas si no se aportan 
+                                esos datos. </p>
+                            <br></br>
+                            <br></br>
+                            <h3 className="flex font-bold italic text-[#256172] mb-2">¿Por cuánto tiempo se conservan los datos personales recabados? </h3>
+                            <p>Los datos personales proporcionados se conservarán mientras se mantenga la relación comercial o no solicites su supresión y durante el plazo 
+                                por el cuál pudieran derivarse responsabilidades legales por los servicios prestados.  </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Legitimación:  </h2>
+                            <p>El tratamiento de tus datos se realiza con las siguientes bases jurídicas que legitiman el mismo: </p>
+                                <div className="flex justify-center items-center">
+                                    <div className="w-[90%] text-justify" >
+                                        <li>La solicitud de información y/o la contratación de los servicios de Stacking Up, cuyos términos y condiciones se pondrán a tu 
+                                            disposición en todo caso, de forma previa a una eventual contratación. </li>
+                                        <li>El consentimiento libre, específico, informado e inequívoco, en tanto que te informamos poniendo a tu disposición la presente 
+                                            política de privacidad, que, tras la lectura de esta, en caso de estar conforme, puedes aceptar mediante una declaración o una 
+                                            clara acción afirmativa, como el marcado de una casilla dispuesta al efecto. </li>
+                                     </div>
+                                </div>
+                            <br></br>
+                            <p>En caso de que no nos facilites tus datos o lo hagas de forma errónea o incompleta, no podremos atender tu solicitud, resultando del todo 
+                                imposible proporcionarte la información solicitada o llevar a cabo la contratación de los servicios.  </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Destinatarios </h2>
+                            <p>Los datos no se comunicarán a ningún tercero ajeno a Stacking Up, salvo obligación legal. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Datos recopilados por usuarios de los servicios  </h2>
+                            <p>En los casos en que el usuario incluya ficheros con datos de carácter personal en los servidores de alojamiento compartido, Stacking Up no se 
+                                hace responsable del incumplimiento por parte del usuario del RGPD. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Retención de datos en conformidad a la LSSI </h2>
+                            <p>Stacking Up informa de que, como prestador de servicio de alojamiento de datos y en virtud de lo establecido en la Ley 34/2002 de 11 de julio de 
+                                Servicios de la Sociedad de la Información y de Comercio Electrónico (LSSI), retiene por un periodo máximo de 12 meses la información 
+                                imprescindible para identificar el origen de los datos alojados y el momento en que se inició la prestación del servicio. La retención de estos 
+                                datos no afecta al secreto de las comunicaciones y sólo podrán ser utilizados en el marco de una investigación criminal o para la salvaguardia 
+                                de la seguridad pública, poniéndose a disposición de los jueces y/o tribunales o del Ministerio que así los requiera. </p>
+                                <br></br>
+                            <p>La comunicación de datos a las Fuerzas y Cuerpos del Estado se hará en virtud a lo dispuesto en la normativa sobre protección de datos 
+                                personales.</p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Derechos propiedad intelectual Stacking Up  </h2>
+                            <p>Stacking Up es titular de todos los derechos de autor, propiedad intelectual, industrial, "know how" y cuantos otros derechos guardan relación 
+                                con los contenidos del sitio web stackingup.es o aplicación móvil y los servicios ofertados en los mismos, así como de los programas necesarios 
+                                para su implementación y la información relacionada.  </p>
+                            <p>No se permite la reproducción, publicación y/o uso no estrictamente privado de los contenidos, totales o parciales, del sitio web stackingup.es o 
+                                aplicación móvil sin el consentimiento previo y por escrito. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] "> Propiedad intelectual del software</h2>
+                            <p>El usuario debe respetar los programas de terceros puestos a su disposición por Stacking Up, aun siendo gratuitos y/o de disposición pública.</p>
+                            <br></br>
+                            <p>Stacking Up dispone de los derechos de explotación y propiedad intelectual necesarios del software. </p>
+                            <br></br>
+                            <p>El usuario no adquiere derecho alguno o licencia por el servicio contratado, sobre el software necesario para la prestación del servicio, ni 
+                                tampoco sobre la información técnica de seguimiento del servicio, excepción hecha de los derechos y licencias necesarios para el cumplimiento de 
+                                los servicios contratados y únicamente durante la duración de estos. </p>
+                                <br></br>
+                            <p>Para toda actuación que exceda del cumplimiento del contrato, el usuario necesitará autorización por escrito por parte de Stacking Up, quedando 
+                                prohibido al usuario acceder, modificar, visualizar la configuración, estructura y ficheros de los servidores propiedad de Stacking Up, asumiendo
+                                 la responsabilidad civil y penal derivada de cualquier incidencia que se pudiera producir en los servidores y sistemas de seguridad como 
+                                 consecuencia directa de una actuación negligente o maliciosa por su parte. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Propiedad intelectual de los contenidos alojados </h2>
+                            <p>Se prohíbe el uso contrario a la legislación sobre propiedad intelectual de los servicios prestados por Stacking Up y, en particular de: </p>
+                                <div className="flex justify-center items-center">
+                                    <div className="w-[90%] text-justify" >
+                                        <li>La utilización que resulte contraria a las leyes españolas o que infrinja los derechos de terceros. </li>
+                                        <li>La publicación o la transmisión de cualquier contenido que, a juicio de Stacking Up, resulte violento, obsceno, abusivo, ilegal, 
+                                            racial, xenófobo o difamatorio. </li>
+                                        <li>Los cracks, números de serie de programas o cualquier otro contenido que vulnere derechos de la propiedad intelectual de 
+                                            terceros. </li>
+                                        <li>La recogida y/o utilización de datos personales de otros usuarios sin su consentimiento expreso o contraviniendo lo dispuesto en Reglamento (UE) 
+                                            2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que respecta al 
+                                            tratamiento de datos personales y a la libre circulación de estos. </li>
+                                        <li>La utilización del servidor de correo del dominio y de las direcciones de correo electrónico para el envío de correo masivo no deseado. </li>
+                                     </div>
+                                </div>
+                            <br></br>
+                            <p>El usuario tiene toda la responsabilidad sobre el contenido de su web, la información transmitida y almacenada, los enlaces de hipertexto, 
+                                las reivindicaciones de terceros y las acciones legales en referencia a propiedad intelectual, derechos de terceros y protección de menores. </p>
+                            <br></br>
+                            <p>El usuario es responsable respecto a las leyes y reglamentos en vigor y las reglas que tienen que ver con el funcionamiento del servicio online, 
+                                comercio electrónico, derechos de autor, mantenimiento del orden público, así como principios universales de uso de Internet.  </p>
+                            <br></br>
+                            <p>El	usuario	indemnizará a Stacking Up por los gastos que generará la imputación de Stacking Up en alguna causa cuya responsabilidad fuera 
+                                atribuible al usuario, incluidos honorarios y gastos de defensa jurídica, incluso en el caso de una decisión judicial no definitiva. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Protección de la información alojada </h2>
+                            <p>Stacking Up realiza copias de seguridad de los contenidos alojados en sus servidores. Sin embargo, no se responsabiliza de la pérdida o el 
+                                borrado accidental de los datos por parte de los usuarios. De igual manera, no garantiza la reposición total de los datos borrados por los 
+                                usuarios, ya que los citados datos podrían haber sido suprimidos y/o modificados durante el periodo del tiempo transcurrido desde la última 
+                                copia de seguridad. </p>
+                                <br></br>
+                            <p>Los servicios ofertados, excepto los servicios específicos de backup, no incluyen la reposición de los contenidos conservados en las copias de 
+                                seguridad realizadas por Stacking Up cuando esta pérdida sea imputable al usuario; en este caso, se determinará una tarifa acorde a la 
+                                complejidad y volumen de la recuperación, siempre previa aceptación del usuario. </p>
+                                <br></br>
+                            <p>La reposición de datos borrados sólo está incluida en el precio del servicio cuando la pérdida del contenido sea debida a causas atribuibles a 
+                                Stacking Up, ya sea por un fallo de seguridad o un mal desarrollo del guardado de datos. En ese caso, se comunicará vía correo electrónico a 
+                                todos los usuarios de la pérdida de los datos y se procederá a una indemnización acorde a la pérdida causada. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Comunicaciones comerciales  </h2>
+                            <p>En aplicación de la LSSI, Stacking Up no enviará comunicaciones publicitarias o promocionales por correo electrónico u otro medio de comunicación 
+                                electrónica equivalente que previamente no hubieran sido solicitadas o expresamente autorizadas por los destinatarios de las mismas. </p>
+                                <br></br>
+                            <p>En el caso de usuarios con los que exista una relación contractual previa, Stacking Up sí está autorizado al envío de comunicaciones comerciales 
+                                referentes a productos o servicios	de Stacking Up que sean similares a los que inicialmente fueron objeto de contratación con el cliente. </p>
+                                <br></br>
+                            <p>En todo caso, el usuario, tras acreditar su identidad, podrá solicitar que no se le haga llegar más información comercial a través de los canales 
+                                de Atención al Cliente. </p>
+                            <br></br>
+                            <br></br>
+
+                            <h2 className="flex font-bold text-[#256172] ">Garantías de privacidad   </h2>
+                            <p>En aplicación de la LSSI, Stacking Up no enviará comunicaciones publicitarias o promocionales por correo electrónico u otro medio de comunicación 
+                                electrónica equivalente que previamenCon el fin de garantizar una comunicación privada de cualquier usuario con los servidores del sitio web 
+                                stackingup.es, Stacking Up se compromete a hacer uso de conexiones HTTPS utilizando el protocolo TLS de versión 1.3, el cual garantiza a todo 
+                                usuario una comunicación cifrada que no exponga ningún dato sensible a terceras personas. </p>
+                                <br></br>
+                            <p>EEl mismo usuario podrá comprobar la validez del certificado digital dentro de la propia página web de Stacking Up.</p>
+                            <br></br>
+                            <br></br>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <Footer />
+        </div>
+    )
+}
